f9e26c2c787bfe03ed85853990d5b7a99bf15698
mistermooster11 <mistermooster11@gmail.com>
2026-05-03 20:10:07 +0700

    update-nav-error

--- lib/constants/mainNavItems.ts
+++ lib/constants/mainNavItems.ts
@@ -1,8 +1,25 @@
+export type DropdownLink = {
+  label: string;
+  href: string;
+  external?: boolean;
+};
+
+export type DropdownQuickLink = DropdownLink & { icon: string };
+
+export type NavDropdown = {
+  title: string;
+  titleHref: string;
+  description: string;
+  links: DropdownLink[];
+  items?: DropdownLink[];
+  quickLinks?: DropdownQuickLink[];
+};
+
 export type MainNavItem = {
   label: string;
   href?: string | null;
   external?: boolean;
-  dropdown?: null;
+  dropdown?: NavDropdown | null;
 };
 
 export const mainNavItems: MainNavItem[] = [
